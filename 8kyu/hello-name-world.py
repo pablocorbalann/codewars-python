33d359aaaaa4f9cbf95296d1ee64789821c7f440
Pablo Corbalán De Concepción <63709477+PabloCorbCon@users.noreply.github.com>
2020-09-25 12:52:49 +0200

    hello-name-world problem solved and pushed to master

---
+++ 8kyu/hello-name-world.py
@@ -0,0 +1,9 @@
+# Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+# Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+# Examples:
+# hello "john"   => "Hello, John!"
+# hello "aliCE"  => "Hello, Alice!"
+# hello          => "Hello, World!" # name not given
+# hello ''       => "Hello, World!" # name is an empty String
+def hello(name=''):
+	return 'Hello, {0}!'.format(name.capitalize() if name else 'World')
